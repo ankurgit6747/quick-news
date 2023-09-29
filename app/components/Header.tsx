1bc8732b028c99c1955dea4668e1081bbf61fee6
ANKUR <ankur02srma@gmail.com>
2023-09-29 19:26:44 +0530

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className='flex justify-center flex-col items-center py-5 gap-2'>
+      <div className='text-3xl' >Quick News</div>
+      <div className='text-xl'>News in shorts</div>
+    </div>
+  )
+}
+
+export default Header;
